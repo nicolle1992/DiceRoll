795755b1f6f0d05b389164736ab1ed17570b0b84
nicolle1992 <80721972+nicolle1992@users.noreply.github.com>
2022-01-27 17:26:33 -0300

    hello

--- app/src/test/java/com/example/diceroll2/MainActivityKtTest.kt
+++ app/src/test/java/com/example/diceroll2/MainActivityKtTest.kt
@@ -4,16 +4,30 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class MainActivityKtTest {
-   @Test
-    fun calculationBonusTest (){
+    @Test
+    fun calculationBonusTest() {
         val result = calculationBonus(150)
         val expected = 60000
-        assertEquals("El resultado es: ",result,expected)
+        assertEquals("El resultado es: ", result, expected)
+    }
+
+    @Test
+    fun calculationHealthTest() {
+        val result = calculationHealth(150, 5000)
+        val expected = 65625.0
+        assertEquals("El resultado es:", result, expected, 0.0)
+    }
+
+    @Test
+    fun calculationPensionFundManagerTest() {
+        val result = calculationPensionFundManager(150, 5000)
+        val expected = 105468.75
+        assertEquals("El resultado es:", result, expected, 0.0)
     }
     @Test
-    fun calculationHealthTest (){
-        val result = calculationHealth(150,5000)
-        val expected = 65625.0
-        assertEquals("El resultado es:", result , expected, 0.0)
+    fun calculationAssetsTest(){
+        val result = calculationAssets(150, 5000)
+        val expected = 937500.0
+        assertEquals("Es igual a : ", result, expected, 0.0)
     }
 }
